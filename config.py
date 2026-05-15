f4a8219e4af1e3ba22ba61420ea32c7e1e312d0f
aniket2git <emailtwoani@gmail.com>
2026-05-15 11:33:15 +0530

    fixed production database config

--- config.py
+++ config.py
@@ -9,9 +9,20 @@ class Config:
 
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url.startswith("postgres://"):
+
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql://",
+            1
+        )
+
+    SQLALCHEMY_DATABASE_URI = database_url
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
